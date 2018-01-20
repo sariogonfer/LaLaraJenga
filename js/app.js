22a442299ea261063690654ed145ab43c460e300
Cesar Gonzalez Fernandez <cesar.gon.fer@gmail.com>
2018-01-20 13:29:23 +0100

    Test with quiz api.

--- js/app.js
+++ js/app.js
@@ -11,19 +11,45 @@ function getRandomChallengeFromList(list) {
 }
 
 function generateChallengeFromList() {
-  return "<p>" + getRandomChallengeFromList(CHALLENGE_LIST) + "</p>";
+  var itemElement = document.createElement("p");
+  var text = document.createTextNode(getRandomChallengeFromList(CHALLENGE_LIST));
+  itemElement.appendChild(text);
+  return itemElement;
+}
+
+function generateQuizChallenge() {
+  var apiUri = "https://opentdb.com/api.php?amount=1";
+  var xmlHttp = new XMLHttpRequest();
+  var quizElement = document.createElement("div");
+
+  xmlHttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      quizElement.innerText = this.responseText;
+    }
+  };
+
+  xmlHttp.open("GET", apiUri, true);
+  xmlHttp.send();
+
+  return quizElement
 }
 
 function getChallengeElement() {
   return document.getElementById("challenge");
 }
 
-function setChallenge(htmlChallenge) {
-  getChallengeElement().innerHTML = htmlChallenge;
+function setChallenge(elemChallenge) {
+  var elem = getChallengeElement();
+  elem.innerHTML = '';
+  getChallengeElement().appendChild(elemChallenge);
+}
+
+function getRandomGenerator() {
+  return  generateQuizChallenge;
 }
 
 function newChallenge() {
-  setChallenge(generateChallengeFromList());
+  setChallenge(getRandomGenerator()());
 }
 
 (function() {
